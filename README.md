5d420f39c10ada6c4f001661763f5e8e8b01406f
Al Tools <email@allen.tools>
2019-06-11 01:42:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Currently Reading https://www.packtpub.com/web-development/flask-building-python-web-services
+![flask_book.png](https://github.com/kayfay/flask-web-services/raw/master/flask_book.png)
---
+++ firstapp/hello.py
@@ -0,0 +1,13 @@
+"""
+module doctstring: Serve up a static "Hello World!" web page
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
