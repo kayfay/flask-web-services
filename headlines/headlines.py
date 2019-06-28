d792080ff411b8974682768ee224ede380ab97d6
Al Tools <email@allen.tools>
2019-06-27 20:58:07 -0400

    Add weather functionality

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -5,6 +5,9 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import urllib
+import urllib2
 
 app = Flask(__name__)
 
@@ -21,7 +24,22 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    weather = get_weather("Jacksonville, Fl")
+    return render_template("home.html", articles=feed['entries'], weather=weather)
+
+def get_weather(query):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid="
+    query = urllib.parse.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        {"description" : parsed["weather"][0]["description"],
+         "temperature" : parsed["main"]["temp"],
+         "city" : parsed["name"]}
+    return weather
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
